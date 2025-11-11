f0c3fadb0ecb00c32af6a07638692261a49af6ba
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2025-11-11 15:22:30 +0100

    formulaire dynamique

--- JS/rendez-vous.js
+++ JS/rendez-vous.js
@@ -6,7 +6,13 @@ const menulist = document.getElementById('menu');
 menuButton.addEventListener('click',()=>{
     menulist.classList.toggle('hidden');
 });
-
+const name_input = document.getElementById('name');
+const email_input = document.getElementById('email');
+const telephone_input = document.getElementById('telephone');
+const date_input = document.getElementById('date');
+const time_input = document.getElementById('time');
+const raison_input = document.getElementById('raison');
+const submit_button = document.getElementById('submit');
 
 const medicin_choisi = document.getElementById('medicins_input');
 const medicin_liste = document.getElementById('medicins_liste');
@@ -19,5 +25,14 @@ medicin_choisi.addEventListener('click',()=>{
     }
     medicin_liste.classList.remove('hidden');
     clicked = true;
-
 })
+
+submit_button.addEventListener('click',() =>{
+    console.log('clicked')
+    if (name_input.value == "" || email_input.value == "" || telephone_input.value == "" || date_input.value == "" || time_input.value == "") {
+        alert('entrez tous les informations');
+        return;
+    }
+})
+
+
